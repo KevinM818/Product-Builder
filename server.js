8f8d284b993e25e5341b94dac87041e1b5d030d0
Kevin Mangal <kmangal818@gmail.com>
2018-12-12 23:31:49 -0500

    Added cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Shopify = require('shopify-api-node');
+const cors = require('cors');
 
 const shopify = new Shopify({
   shopName: 'app-test-store-km.myshopify.com',
@@ -10,6 +11,7 @@ const shopify = new Shopify({
 
 const port = process.env.PORT || 3000;
 var app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/create-product', (req, res) => {
